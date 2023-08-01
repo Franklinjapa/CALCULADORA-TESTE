bd4c1fb5357c2e2a89c613bed1b4a02754abcdc7
Franklinjapa <140675725+Franklinjapa@users.noreply.github.com>
2023-08-01 14:17:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '💩';
+}
+
+// Vai lá, digita uns números aí pra ver o que acontece, kkkk!
